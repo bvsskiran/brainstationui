6c67e409f59a24227d0a8c4356f69e065ca176bc
Kiran <bvsskiran@gmail.com>
2025-08-31 20:18:10 +0530

    Logo warning resolved

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,8 +1,7 @@
-//import heroImg1 from '../../../assets/images/brainstation-logo-with-name.png';
-//import heroImg1 from '../../assets/images/brainstation-logo-with-name.jpg';
+import Image from 'next/image';
 const Logo = () => (
   <span className="ml-2 flex items-center h-12 whitespace-nowrap text-2xl font-bold text-gray-900 dark:text-white md:text-xl">
-    <img
+    <Image
       className="h-10 w-auto max-w-[160px] align-middle"
       src="/images/brainstation-logo-with-name.jpg"
       alt="BrainStation"
